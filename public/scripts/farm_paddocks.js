c439499eb02e9d8c440cf21de0f39460a51e957d
Justin Pishief <justin.pishief@justcreateit.nz>
2017-11-09 12:08:33 +1300

    Updated paddock selection and associated crop logic

--- public/scripts/farm_paddocks.js
+++ public/scripts/farm_paddocks.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
 		// update on change (when multiple paddocks available)
 		var f_id = $( "#farm_id option:selected" ).val();
 		var p_id = $( "#paddock_id option:selected" ).val();
-		addSelectOptions(f_id);
+		addSelectOptions(f_id , p_id);
 		$(function() {
 			// make me selected
 			$("#farm_id").val(f_id);
@@ -35,11 +35,10 @@ function removeChildren(){
 }
 
 /* add options to farm, paddock and crop select boxes */
-function addSelectOptions(f_id){ 
+function addSelectOptions(f_id, p_id){ 
 	
 	// Remove all select options then add new paddock,crops specific to selected farm_id 
 	removeChildren();
-
 	// selectable options
 	var farm_options= $("#farm_id");
 	var paddock_options = $("#paddock_id");
@@ -50,16 +49,26 @@ function addSelectOptions(f_id){
 	$.each(farms, function (key, value){		
 		farm_options.append($("<option />").val(value.farm.farm_id).text(value.farm.farm_name));
 		if (value.farm.farm_id == f_id) { // selected farm
-			var paddocks = value.farm.paddocks
-			$.each(paddocks, function (key, value){				
-				if ( ($.isEmptyObject(value.crops) == false) || $("#page_function").val() == "create_crop" ) {
-					var crops = value.crops;
-					$.each(crops, function (key, value){					
-						crop_options.append($("<option />").val(value.crop_id).text(value.crop_plant_date)); 
-					});
-					paddock_options.append($("<option />").val(value.paddock_id).text(value.paddock_name));
-					$("#paddock_area").val(value.paddock_area);
+			var paddocks = value.farm.paddocks;
+			// check to see if paddock select option changed
+			if (p_id == null) { // default/initial state - no change
+				// set to the first paddock associated with this farm
+				p_id = paddocks[0].paddock_id; 				
+			}
+			$.each(paddocks, function (key, value){					
+				if ( (value.paddock_id == p_id ) ) { // currently selected paddock
+					if ( ($.isEmptyObject(value.crops) == false) || $("#page_function").val() == "create_crop" ) {
+						// add associated crop plant date
+						var crops = value.crops;
+						$.each(crops, function (key, value){					
+							crop_options.append($("<option />").val(value.crop_id).text(value.crop_plant_date)); 
+						});
+						// add associated paddock areas
+						$("#paddock_area").val(value.paddock_area);	
+					}
 				}
+				// add farms associated paddock selection values
+				paddock_options.append($("<option />").val(value.paddock_id).text(value.paddock_name));			
 			});		
 		}
 	});
