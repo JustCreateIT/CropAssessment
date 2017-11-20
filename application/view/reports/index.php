4acd578e3637eb7e7195dcd34696d2a6dce0d52a
Justin Pishief <justin.pishief@justcreateit.nz>
2017-11-21 12:31:50 +1300

    Changed reports interface to use image buttons

--- application/view/reports/index.php
+++ application/view/reports/index.php
@@ -20,12 +20,39 @@
 						<select name="crop_id" id="crop_id"></select>
 					</div>
 				</div>
+				<button type="submit" name="reports" value="View Emergence Assessment" id="emergence"><figure class="item">
+					<img src="<?php echo Config::get('URL'); ?>images/reports_view_emergence.png"/>
+					<figcaption class="responsive caption" data-min="10" data-max="18">View Emergence Assessment</figcaption>
+				</figure></button>
+				<button type="submit" name="reports" value="View Three Leaf Assessment" id="threeleaf"><figure class="item">
+					<img src="<?php echo Config::get('URL'); ?>images/reports_view_three_leaf.png"/>
+					<figcaption class="responsive caption" data-min="10" data-max="18">View Three Leaf Assessment</figcaption>
+				</figure></button>
+				<button type="submit" name="reports" value="View Five Leaf Assessment" id="fiveleaf"><figure class="item">
+					<img src="<?php echo Config::get('URL'); ?>images/reports_view_five_leaf.png"/>
+					<figcaption class="responsive caption" data-min="10" data-max="18">View Five Leaf Assessment</figcaption>
+				</figure></button>
+				<button type="submit" name="reports" value="View Bulbing Assessment" id="bulbing"><figure class="item">
+					<img src="<?php echo Config::get('URL'); ?>images/reports_view_bulbing.png"/>
+					<figcaption class="responsive caption" data-min="10" data-max="18">View Bulbing Assessment</figcaption>
+				</figure></button>
+				<button type="submit" name="reports" value="View Harvest Assessment" id="harvest" style="margin-bottom:10px"><figure class="item">
+					<img src="<?php echo Config::get('URL'); ?>images/reports_view_harvest.png"/>
+					<figcaption class="responsive caption" data-min="10" data-max="18">View Harvest Assessment</figcaption>
+				</figure></button>
+				<button type="submit" name="reports" value="View Paddock Survey" id="survey" style="margin-bottom:10px"><figure class="item">
+					<img src="<?php echo Config::get('URL'); ?>images/paddock_view.png"/>
+					<figcaption class="responsive caption" data-min="10" data-max="18">View Survey Map</figcaption>
+				</figure></button>						
+
+				<!--
 				<input type="submit" name="reports" value="View Emergence Assessment" id="emergence"/>
 				<input type="submit" name="reports" value="View Three Leaf Assessment" id="threeleaf"/>
 				<input type="submit" name="reports" value="View Five Leaf Assessment" id="fiveleaf"/>
 				<input type="submit" name="reports" value="View Bulbing Assessment" id="bulbing"/>
 				<input type="submit" name="reports" value="View Harvest Assessment" id="harvest"/>
 				<input type="submit" name="reports" value="View Paddock Survey" id="survey"/>
+				-->
 			<?php foreach ($this->report_info as $report_info) { ?>
 				<input type="hidden" name="<?= $report_info->paddock_name; ?>" id="<?= $report_info->paddock_id; ?>" value="<?= implode(',',$report_info->growth_stage_id); ?>">
 			<?php } ?>
