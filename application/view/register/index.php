86f4b0b181aac428a143cf94c29a4391d3564ed1
Justin Pishief <justin.pishief@justcreateit.nz>
2017-12-01 23:33:49 +1300

    Added role option to registration page

--- application/view/register/index.php
+++ application/view/register/index.php
@@ -6,17 +6,31 @@
     <!-- login box on left side -->
     <!--<div class="login-box" style="width: 50%; display: block;">-->
 	<div class="login-box">
-        <h2>Register a new account</h2>
+        <h1>Register a new account</h1>
 
         <!-- register form -->
         <form method="post" action="<?php echo Config::get('URL'); ?>register/register_action">
             <!-- the user name input field uses a HTML5 pattern check -->
             <!--<input type="text" pattern="[a-zA-Z0-9]{2,64}" name="user_name" placeholder="Username (letters/numbers, 2-64 chars)" required />-->
+						<div>
+			<label style="width:100%;text-transform:none;margin-bottom:10px;" for="user_account_type">Select your role on the farm:</label>
+			</div>
+			<div>				
+				<select style="width:100%;padding:15px; margin-bottom:10px;" name="user_account_type" id="user_account_type">
+					<?php foreach ($this->account_types as $account_type) { 							
+						if ($account_type->account_type == 5) { ?> 
+						<option value="<?= $account_type->account_type ?>" selected>Employee / Contractor</option>
+						<?php } 
+						if ($account_type->account_type == 9) { ?> 
+						<option value="<?= $account_type->account_type ?>">Farm Owner / Manager</option>
+					<?php } 				
+					} ?></select>
+			</div>
 			<input type="text" name="user_first_name" placeholder="First name" required />
 			<input type="text" name="user_last_name" placeholder="Last name" required />			
-            <input type="text" name="user_email" placeholder="Email address (a real address)" required />
-            <input type="text" name="user_email_repeat" placeholder="Repeat email address (to prevent typos)" required />
-			<input type="text" name="user_phone_number" placeholder="Phone number" />
+            <input type="email" name="user_email" placeholder="Email address (a real address)" required />
+            <input type="email" name="user_email_repeat" placeholder="Repeat email address (to prevent typos)" required />
+			<input type="number" name="user_phone_number" placeholder="Phone number" />
             <input type="password" name="user_password_new" pattern=".{8,}" placeholder="Password (8+ characters)" required autocomplete="off" />
             <input type="password" name="user_password_repeat" pattern=".{8,}" required placeholder="Repeat your password" autocomplete="off" />
 
