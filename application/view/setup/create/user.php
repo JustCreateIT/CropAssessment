3601082c4715f081226e6c4ae2a6879c66f7803d
Justin Pishief <justin.pishief@justcreateit.nz>
2017-12-01 23:34:53 +1300

    Cosmetic changes to user views

--- application/view/setup/create/user.php
+++ application/view/setup/create/user.php
@@ -1,45 +1,46 @@
 <div class="container">
     <h1>Add A New Farm User</h1>
 	<div class="input-box">
-		<!-- echo out the system feedback (error and success messages) -->
+		<!-- echo out the system feedback (error and success messages) -->		
         <div id="feedback_messages"></div>
-		<form id="addFarmUser_action" method="post" action="<?php echo Config::get('URL');?>setup/addFarmUser_action">        
-        <div><label for="farm_id">Select Farm</label><select name="farm_id" id="farm_id"></select></div>
-		<h2>User Information</h2>		
-        <p>
-           <p>
-				<label>First name</label>
-				<input type="text" placeholder="First name" name="user_first_name" value="" required />
-			</p>
-			<p>
-				<label>Last name</label>
-				<input type="text" placeholder="Last name" name="user_last_name" value="" required />
-			</p>
-			<p>
-				<label>Email address</label>
-				<input type="text" placeholder="User email address (a real address)" name="user_email" value="" required />
-			</p>
-			<p>
-				<label>Confirm Email address</label>
-				<input type="text" placeholder="Repeat email address (to prevent typos)" name="user_email_repeat" value="" required />
-			</p>			
-			<p>
-				<label>Phone number</label>
-				<input type="text" placeholder="Phone number" name="user_phone_number" value="" />
-			</p>
-	
-			<div class="submit_float_left"> 				
-				<div class="app-button" style="margin:0; padding:0;">                
-					<a href="<?php echo Config::get('URL'); ?>setup">Back</a>
+		<div class="form_input">
+			<form id="addFarmUser_action" method="post" action="<?php echo Config::get('URL');?>setup/addFarmUser_action">        
+			<div><label for="farm_id">Select Farm</label><select name="farm_id" id="farm_id"></select></div>
+				<div>
+				<h2>User Information</h2>
 				</div>
+			   <div>
+					<label>First name</label>
+					<input type="text" placeholder="First name" name="user_first_name" value="" required />
+				</div>
+				<div>
+					<label>Last name</label>
+					<input type="text" placeholder="Last name" name="user_last_name" value="" required />
+				</div>
+				<div>
+					<label>Email address</label>
+					<input type="text" placeholder="User email address (a real address)" name="user_email" value="" required />
+				</div>
+				<div>
+					<label>Confirm Email address</label>
+					<input type="text" placeholder="Repeat email address (to prevent typos)" name="user_email_repeat" value="" required />
+				</div>			
+				<div>
+					<label>Phone number</label>
+					<input type="text" placeholder="Phone number" name="user_phone_number" value="" />
+				</div>
+				<div>
+					<span ><label for="send_details_self" style="width:280px; margin: 0 auto;">Send yourself email confirmation:</label><input type="checkbox" name="send_details_self" id="send_details_self" value="" checked></span>
+				</div>
+				<div class="submit" >
+					<input class="submit" type="submit" id="btnSubmit" value="Send User Request" <?php if((Session::get("user_account_type") == 1 || Session::get("user_account_type") == 5)){ echo 'disabled style="color: #ccc; background-color: transparent; border: 2px solid #ccc;"'; } ?>/>
+				</div>
+			</form>		
+			<div class="app-button" style="margin:0 auto;">                
+				<a href="<?php echo Config::get('URL'); ?>setup">Back</a>
 			</div>
-			<div class="submit_float_left">
-				<input class="disabled" type="submit" id="btnSubmit" value="Send User Request" <?php if((Session::get("user_account_type") == 1 || Session::get("user_account_type") == 5)){ echo 'disabled style="color: #ccc; background-color: transparent; border: 2px solid #ccc;"'; } ?>/>
-			</div>
-			<div style="clear:both;"></div>
-		</form>
-		<input type="hidden" name="farm_details" id="farm_details" value='<?php echo $this->farm_details; ?>'>			
-		</p>
+			<input type="hidden" name="farm_details" id="farm_details" value='<?php echo $this->farm_details; ?>'>			
+		</div>
     </div>
 </div>
 <div id="loader_icon" style="display:none;"><img src="<?php echo Config::get('URL'); ?>images/ajax-loader.gif" /></div>
